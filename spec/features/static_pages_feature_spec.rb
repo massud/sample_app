91d291f06ea126bab537338e1e6b97408790dfb6
massud <massudgnan@gmail.com>
2015-06-15 19:07:49 +0100

    finish layouts and routes

--- spec/features/static_pages_feature_spec.rb
+++ spec/features/static_pages_feature_spec.rb
@@ -4,54 +4,69 @@ feature 'Static Pages' do
 
   describe 'Home page' do
 
+    before { visit root_path }
+
     it 'should have_title("Ruby on Rails Tutorial Sample App | Home")' do
-      visit '/'
       expect(page).to have_title("Ruby on Rails Tutorial Sample App")
     end
   
 
     it "should have the h1 'Sample App'" do
-      visit '/'
       expect(page).to have_selector('h1', :text => 'Sample App')
     end
   end
 
   describe "Help page" do
 
+    before { visit help_path }
+
     it "should have content 'Help'" do 
-      visit '/static_pages/help' 
       expect(page).to have_content 'Help'
     end 
 
     it 'should have_title("Ruby on Rails Tutorial Sample App | Help")' do
-      visit '/static_pages/help'
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
     end
   end
 
   describe 'About page' do
 
+    before { visit about_path }
+
     it "should have content 'About'" do
-      visit '/static_pages/about'
       expect(page).to have_content 'About'
     end
 
     it 'should have_title("Ruby on Rails Tutorial Sample App | About")' do
-      visit '/static_pages/about'
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
     end
   end
 
   describe 'Contact page' do
 
+    before { visit contact_path }
+
+
     it "should have content 'Contact'" do
-      visit '/static_pages/contact'
       expect(page).to have_content 'Contact'
     end
 
     it 'should have_title("Ruby on Rails Tutorial Sample App | Contact")' do
-      visit '/static_pages/contact'
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
+    end
+  end
+
+   describe 'Sign up page' do
+
+    before { visit signup_path }
+
+
+    it "should have content 'This will be a signup page for new users'" do
+      expect(page).to have_content 'This will be a signup page for new users'
+    end
+
+    it 'should have_title("Sign up")' do
+      expect(page).to have_title("Sign up")
     end
   end
 

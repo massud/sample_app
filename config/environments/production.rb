d5fc8144994364ebc73858e45f8e23d86d154293
massud <massudgnan@gmail.com>
2015-06-26 17:48:26 +0100

    slight changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,6 +48,10 @@ Rails.application.configure do
   # when problems arise.
   config.log_level = :debug
 
+  config.action_mailer.default_url_options = { :host => 'sample-app-01-2015.herokuapp.com' }
+
+
+
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
 
